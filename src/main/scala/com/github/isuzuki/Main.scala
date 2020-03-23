866567d051ec15368617a15978d46c3c31212713
isuzuki <ikkuntech@gmail.com>
2020-03-24 04:09:27 +0900

    added EitherT sample

--- src/main/scala/com/github/isuzuki/Main.scala
+++ src/main/scala/com/github/isuzuki/Main.scala
@@ -1,7 +1,8 @@
 package com.github.isuzuki
 
-import cats.data.OptionT
-import cats.effect.IO
+import cats.data.{EitherT, OptionT}
+import cats.effect.ExitCase.Error
+import cats.effect.{ExitCase, IO}
 import doobie.free.connection.ConnectionIO
 import doobie.implicits._
 import doobie.util.transactor.Transactor
@@ -26,4 +27,10 @@ object Main extends App {
     result <- OptionT.liftF(repository.update(item.copy(name = "bar")))
   } yield result
   io.value.transact(xa).unsafeRunSync().foreach(println)
+
+  val io2 = for {
+    item <- EitherT.fromOptionF(repository.findById("hoge"), Error("not found."))
+    result <- EitherT.liftF[ConnectionIO, ExitCase[String], Int](repository.update(item.copy(name = "baz")))
+  } yield result
+  io2.value.transact(xa).unsafeRunSync().foreach(println)
 }

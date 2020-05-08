d21f2d6b2a7d11ba59c6e466d00a8b8a48474407
isuzuki <ikkuntech@gmail.com>
2020-05-08 18:46:46 +0900

    added use hikari transactor sample

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ val doobieVersion = "0.8.8"
 
 libraryDependencies ++= Seq(
   "org.tpolecat" %% "doobie-core" % doobieVersion,
+  "org.tpolecat" %% "doobie-hikari" % doobieVersion,
   "org.tpolecat" %% "doobie-postgres" % doobieVersion
 )
--- src/main/scala/com/github/isuzuki/Main.scala
+++ src/main/scala/com/github/isuzuki/Main.scala
@@ -2,31 +2,43 @@ package com.github.isuzuki
 
 import cats.data.{EitherT, OptionT}
 import cats.effect.ExitCase.Error
-import cats.effect.{ExitCase, IO}
+import cats.effect.{Blocker, ContextShift, ExitCase, IO, Resource}
 import doobie.free.connection.ConnectionIO
+import doobie.hikari.HikariTransactor
 import doobie.implicits._
-import doobie.util.transactor.Transactor
+import doobie.util.ExecutionContexts
 
 import scala.concurrent.ExecutionContext
 
 object Main extends App {
-  lazy val repository = new PostgreSQLItemRepository
+  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  val transactor: Resource[IO, HikariTransactor[IO]] =
+    for {
+      ce <- ExecutionContexts.fixedThreadPool[IO](32) // our connect EC
+      be <- Blocker[IO] // our blocking EC
+      xa <- HikariTransactor.newHikariTransactor[IO](
+        "org.postgresql.Driver",
+        "jdbc:postgresql://localhost:5432/doobie",
+        "doobie",
+        "doobie",
+        ce, // await connection here
+        be // execute JDBC operations here
+      )
+    } yield xa
 
-  implicit val cs = IO.contextShift(ExecutionContext.global)
-  val xa = Transactor
-    .fromDriverManager[IO]("org.postgresql.Driver", "jdbc:postgresql://localhost:5432/doobie", "doobie", "doobie")
+  lazy val repository = new PostgreSQLItemRepository
 
   val io = for {
     item <- OptionT(repository.findById("hoge"))
     result <- OptionT.liftF(repository.update(item.copy(name = "bar")))
   } yield result
-  io.value.transact(xa).unsafeRunSync().foreach(println)
+  transactor.use(xa => io.value.transact(xa)).unsafeRunSync().foreach(println)
 
   val io2 = for {
     item <- EitherT.fromOptionF(repository.findById("hoge"), Error("not found."))
     result <- EitherT.liftF[ConnectionIO, ExitCase[String], Int](repository.update(item.copy(name = "baz")))
   } yield result
-  io2.value.transact(xa).unsafeRunSync().foreach(println)
+  transactor.use(xa => io2.value.transact(xa)).unsafeRunSync().foreach(println)
 
-  repository.getAll.transact(xa).unsafeRunSync().foreach(println)
+  transactor.use(xa => repository.getAll.transact(xa)).unsafeRunSync().foreach(println)
 }
